75adabb52947dc4a94b3d0957a993f15bf7efe94
liulnn <liulnn@126.com>
2015-05-12 11:14:51 +0800

    Update backend.py

--- djmemcache/backend.py
+++ djmemcache/backend.py
@@ -2,14 +2,19 @@ try:
     import cPickle as pickle
 except ImportError:
     import pickle
-
+import six
 from . import client
 from pymemcache.client import PooledClient
 from django.core.cache.backends.memcached import BaseMemcachedCache
 
+unserialize_types = []
+unserialize_types += six.integer_types
+unserialize_types.extend(six.string_types)
+unserialize_types.append(six.text_type)
+unserialize_types.append(six.binary_type)
 
 def serialize_pickle(key, value):
-    if isinstance(value, str):
+    if isinstance(value, tuple(unserialize_types)):
         return value, 1
     return pickle.dumps(value), 2
 
