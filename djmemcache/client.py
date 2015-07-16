c6f64a1a1b4cf4a6505af72029ea56d3b65e5be9
duanhongyi <duanhongyi@doopai.com>
2015-07-16 10:00:36 +0800

    change disconnect_all

--- djmemcache/client.py
+++ djmemcache/client.py
@@ -1,9 +1,12 @@
-from pymemcache import sharding_client as client
+from pymemcache import sharding_client as _client
 
 
-class Client(client.ShardingClient):
+class Client(_client.ShardingClient):
     # this just fixes some API holes between python-memcached and pymemcache
-    set_multi = client.ShardingClient.set_many
-    get_multi = client.ShardingClient.get_many
-    delete_multi = client.ShardingClient.delete_many
-    disconnect_all = client.ShardingClient.quit
+    set_multi = _client.ShardingClient.set_many
+    get_multi = _client.ShardingClient.get_many
+    delete_multi = _client.ShardingClient.delete_many
+
+    def disconnect_all(self, force=False):
+        if force == True:
+            self.quit()
