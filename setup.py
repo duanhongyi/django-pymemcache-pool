f9437bd82a73b634d6e9289b0f2f8c21ffcf8ce4
duanhongyi <duanhongyi@doopai.com>
2018-10-08 17:52:15 +0800

    change version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requires = ['nose']
 
 
 setup(name='djmemcache',
-      version='0.9',
+      version='0.10',
       description='A Memcached Pool for Django',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
